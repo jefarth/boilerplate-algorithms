b6503803a3afcf68e1f40f9ac7b0f958067394be
Jeremiah Farthing <jeremiah.farthing@gmail.com>
2022-09-11 22:09:24 -0600

    Corrected faster answer and updated comments

--- 04-Server-Side-APIs/01-is-palindrome/Solved/is-palindrome.js
+++ 04-Server-Side-APIs/01-is-palindrome/Solved/is-palindrome.js
@@ -1,5 +1,6 @@
 // Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
 
+// Slower and less performant
 var isPalindrome = function(str) {
   var reversedString = str
     .split("")
@@ -15,9 +16,11 @@ var isPalindrome = function(str) {
 
 // Alternatively, this problem could have been solved by comparing the letters at the front of `str` to the letters at the back of `str`
 
+// Faster and covers special characters
+
 // var isPalindrome = function (str) {
 //   for (var i = 0; i < str.length; i++) {
-//     if (str[i] !== str[str.length - 1 - i]) {
+//     if (str[i].toLowerCase() !== str[str.length-(i+1)]toLowerCase()) {
 //       return false;
 //     }
 //   }
